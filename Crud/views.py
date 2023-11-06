e97cbf1d43bbc5b8b8cd31015e5af5f1a8ebfcc0
gk-george <kimamogeorge100@gmail.com>
2023-11-06 11:04:39 +0300

    updates

---
+++ Crud/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    task=Task.objects.all()
+    return render(request, 'index.html',{'task':task})
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+            form = TaskForm()
+    return render(request,'task_form.html', {'form':form})
+def update_task(request,id):
+    task=get_object_or_404(Task,id=id)
+    if request.method == 'POST':
+        form=TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+            form=TaskForm(instance=task)
+    return render(request, 'task_form.html', {'form':form})
+def delete_task(request, id):
+    task=get_object_or_404(Task,id=id)
+    task.delete()
+    return redirect('task_list')
+
